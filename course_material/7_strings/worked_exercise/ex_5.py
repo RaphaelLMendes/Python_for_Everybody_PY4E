4a711b07c6343699e77fa800cc9ad90ae32e3d4c
Raphael Lucas Mendes <rapmendes@deloitte.com>
2022-02-25 11:39:46 -0300

    completed module 7

---
+++ course_material/7_strings/worked_exercise/ex_5.py
@@ -0,0 +1,21 @@
+'''
+Exercise 5: Take the following Python code that stores a string:
+
+str = 'X-DSPAM-Confidence:0.8475'
+
+Use find and string slicing to extract the portion of the string after 
+the colon character and then use the float function to convert the 
+extracted string into a floating point number.
+'''
+
+string = 'X-DSPAM-Confidence:0.8475'
+
+col_pos = string.find(':')
+
+number = float(string[col_pos+1:])
+
+print('number:',number)
+print('type:',type(number))
+
+
+
