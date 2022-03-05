93cd0b5dd143fa4e9397402a998b19cec512792c
Raphael Lucas Mendes <rapmendes@deloitte.com>
2022-03-04 21:38:54 -0300

    completed module 9 - lists

---
+++ course_material/9_lists/exercises/ex_6.py
@@ -0,0 +1,36 @@
+'''
+Exercise 6: Rewrite the program that prompts the user for a list of numbers 
+and prints out the maximum and minimum of the numbers at the end when the 
+user enters “done”. Write the program to store the numbers the user enters in 
+a list and use the max() and min() functions to compute the maximum and 
+minimum numbers after the loop completes.
+
+Enter a number: 6
+Enter a number: 2
+Enter a number: 9
+Enter a number: 3
+Enter a number: 5
+Enter a number: done
+Maximum: 9.0
+Minimum: 2.0
+'''
+
+numbers = []
+
+while True:
+    num_input = input('Enter a number: ')
+    
+    if num_input == "done":
+        break
+
+    try:
+        number = float(num_input)
+        numbers.append(number)
+    except:
+        print('Invalid input!')
+        continue
+
+
+print(f'''Maximum: {max(numbers)}
+Minimum: {min(numbers)}''')
+
